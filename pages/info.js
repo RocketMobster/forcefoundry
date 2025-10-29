e270c7ea1b020d2a6e2047f7a855af384864d92f
RocketMobster <cbtechdesign@gmail.com>
2025-10-29 11:38:03 -0400

    Update info page version to v2.5.0

--- pages/info.js
+++ pages/info.js
@@ -14,7 +14,7 @@ export default function Info() {
             <div className="space-y-3">
               <div>
                 <span className="text-gray-400">Version:</span>
-                <span className="ml-2 text-white font-mono bg-gray-700 px-2 py-1 rounded">v2.1.0</span>
+                <span className="ml-2 text-white font-mono bg-gray-700 px-2 py-1 rounded">v2.5.0</span>
               </div>
               <div>
                 <span className="text-gray-400">Developer:</span>
